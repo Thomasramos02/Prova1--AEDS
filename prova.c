690b0bd5a45188525a117b723740bdb755a2730f
Thomas Ramos Oliveira <161534687+Thomasramos02@users.noreply.github.com>
2024-04-23 10:41:56 -0300

    Atualizar o prova.c

--- prova.c
+++ prova.c
@@ -1,8 +1,6 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 
 int main() {
     srand(time(NULL));
@@ -14,32 +12,36 @@ int main() {
     int portaEscolhida;
     char opcao;
     
+    printf("Escolha uma porta (1, 2, 3): ");
+    scanf("%d", &portaEscolhida);
+    
+    // Simulating the opening of the empty door
+    do {
+        portaVazia = rand() % 3 + 1;
+    } while (portaVazia == portaPremiada || portaVazia == portaEscolhida);
+    
+    printf("O apresentador abriu a porta que está vazia.\n");
+    printf("Você deseja trocar para a outra porta? (s/n): ");
+    scanf(" %c", &opcao); // Added space before %c to consume any leading whitespace
+    
+    if (opcao == 's') {
+        printf("Você trocou de porta.\n");
+        if (portaPremiada == portaEscolhida) {
+            printf("Você ganhou!\n");
+        } else {
+            printf("Você perdeu.\n");
+        }
+    } else {
+        printf("Você manteve a porta.\n");
+        if (portaPremiada == portaEscolhida) {
+            printf("Você ganhou!\n");
+        } else {
+            printf("Você perdeu.\n");
+        }
+    }
+    
+    return 0;
+}
 
-    printf("Escolha uma porta 1 , 2 , 3: ");
-    scanf("%d",&portaEscolhida);
-    
-    printf("O apresentador abriu a porta que está vazia")
-    printf("Você deseja trocar para a outra porta? (s/n): ")
-    scanf("%c",&opcao);
-    
-    if(opcao == 's'){
-        printf("voce trocou de porta")
-            if(portaPremiada == PortaEscolhido){
-        printf("Voce ganhou")
-    }else{
-        printf("Voce perdeu")
-    }else{
-        printf("voce manteve a porta")
-        if(portaPremiada == PortaEscolhido){
-        printf("Voce ganhou")
-    }else{
-        printf("Voce perdeu")
-    }
-    }
-    
-    }
-    
-    
-    
 
     
